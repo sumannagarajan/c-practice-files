580cd31ad1f717d7efb5dcceff7fb3a0b3750579
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 23:14:30 +0530

    comparition operator

---
+++ comparision operator.c
@@ -0,0 +1,20 @@
+// COMPARITION OPERATOR 
+
+//THIS IS VERY IMPORATANT FOR PROGRAMMING TO MAKE DECISION 
+
+//THIS IS RETURN BOOLEAN VALUES 0 or 1 TRUE(1) or FALSE(0)
+
+
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    int b=15;
+    printf("TO CHECK GREATTER VALUE : %d\n",a>b);
+    printf("TO CHECK LESSER VALUE : %d\n",a<b);
+    printf("%d\n",a<=b);
+    printf("%d\n",a>=b);
+    printf("%d\n",a==b);
+    printf("%d\n",a!=b);
+    return 0;
+}
