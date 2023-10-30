77d0e21677f184f268bbf70a02a44e43060b68e7
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 19:05:44 +0530

    type conversion

---
+++ type_conversion.c
@@ -0,0 +1,11 @@
+//TYPE CONVERSION
+
+#include<stdio.h>
+int main()
+{
+    int a=99 , b=2;
+    float div_value = a/b;
+    printf("THE DIV VALUE IS : %f\n",div_value); // output = 49.000000
+    return 0;
+
+}
