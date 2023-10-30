e85e128e52d32609192c467df3f2b293c80269cd
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 18:44:36 +0530

    TO DISPLAY SELECTIVE DECIMAL NUM AFTER POINT

--- declareing other types of varible.c
+++ declareing other types of varible.c
@@ -9,7 +9,7 @@ int mynum = 10;
 float f = 90.34;
 char myletter = 's';
 printf("To print int value : %d\n",mynum);
-printf("To print float value : %f\n",f);
+printf("To print float value : %.2f\n",f); //TO DISPLAY SLECTIVE DECIMAL NUMBERS AFTER POINT 
 printf("To print char value : %c\n",myletter);
 return 0;
 }
