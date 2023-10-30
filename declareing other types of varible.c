f7f8c2145a02411e3f1d4a3680e434ffd02496f2
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 18:23:21 +0530

    declare different type of variables

---
+++ declareing other types of varible.c
@@ -0,0 +1,15 @@
+
+// different types of variables 
+
+
+#include<stdio.h>
+int main()
+{
+int mynum = 10;
+float f = 90.34;
+char myletter = 's';
+printf("To print int value : %d\n",mynum);
+printf("To print float value : %f\n",f);
+printf("To print char value : %c\n",myletter);
+return 0;
+}
