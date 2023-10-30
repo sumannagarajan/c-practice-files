25f43902f552687cee77381a87b9b4addd441241
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 20:01:25 +0530

    arithmetic operators

---
+++ arithmatic operators.c
@@ -0,0 +1,14 @@
+// ARITHMETIC OPERATION 
+#include<stdio.h>
+int main()
+{
+    int a=14;
+    int b=2;
+    printf("TO ADD THE VALUE OF A + B : %d\n",a+b); // PERFORM ADD OPER
+    printf("TO MUL THE VALUE OF A * B : %d\n",a*b); // PERFORM MUL OPER
+    printf("TO DIV THE VALUE OF A / B : %d\n",a/b); // PERFORM DIV OPER
+    printf("TO MODLUO THE VALUE OF A %% B : %d\n",a%b); // PERFORM MODULO OPER
+    printf("TO SUB THE VALUE OF A - B : %d\n",a-b); //PERFORM SUB OPER
+    return 0;
+
+}
