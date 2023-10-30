39085536538ca49ed8e92dbbdd529a56f36535a0
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 17:25:53 +0530

    code for declaring variables and assign the values

---
+++ declaring variables.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    int myvariable = 15; //declare variable and assign value
+    printf("print the value store in the my variable : " "%d\n", myvariable); //must want to specify the format specfier 
+    return 0;
+}
