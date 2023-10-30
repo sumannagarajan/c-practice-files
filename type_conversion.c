42a25dbee389a7af57696381babcf12d53cc4e13
sumannagarajan <sumannagarajan@proton.me>
2023-10-30 19:10:04 +0530

    type converion added int to float data type

--- type_conversion.c
+++ type_conversion.c
@@ -4,8 +4,8 @@
 int main()
 {
     int a=99 , b=2;
-    float div_value = a/b;
-    printf("THE DIV VALUE IS : %f\n",div_value); // output = 49.000000
+    float div_value = (float) a/b; // type conversion integer to float data type
+    printf("THE DIV VALUE IS : %.1f\n",div_value); // output = 49.000000
     return 0;
 
 }
