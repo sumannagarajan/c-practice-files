5c27163836db96b7e0db8d1df8dd7ca707f9a315
sumannagarajan <sumannagarajan@proton.me>
2023-10-31 20:57:05 +0530

    while loop

---
+++ while loop.c
@@ -0,0 +1,14 @@
+// while loop
+
+#include<stdio.h>
+int main ()
+{
+    int mynum = 1;
+    while (mynum<100)
+    {
+     printf("To print 10th tables : %d\n",mynum*10);
+     mynum++;
+    }
+    printf("%d\n",mynum); // To display why loop is stoped and TO display mynum value
+    return 0;
+}
