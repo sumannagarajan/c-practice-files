2863a8e7013bcb7c537d93118474e43ac3b93129
sumannagarajan <sumannagarajan@proton.me>
2023-10-31 13:42:58 +0530

    simple ifelse ageing for voteing

---
+++ simple if else.c
@@ -0,0 +1,20 @@
+// THIS IS SIMPLE if else STATEMENT 
+
+// VOTING AGE IS ELIGIBLE OR NOT 
+ 
+ #include<stdio.h>
+ int main ()
+ {
+    int my_age = 17;
+    int voteing_age = 18;
+    if (my_age>=voteing_age)
+    {
+        printf("YES YOU ARE ELIGIBLE FOR VOTE .\n");
+    }
+    else{
+        printf("!!YOU ARE NOT ELIGIBLE FOR VOTE !!! \n");
+        printf(" ----- YOUR AGE IS NOT GREATER THAN OR EQUAL TO VOTING AGE ----- \n");
+    }
+    return 0;
+ }
+    
