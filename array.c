505101c14fee58ae3e48554ecb29bc41a08b1d87
sumannagarajan <sumannagarajan@proton.me>
2023-11-02 20:03:58 +0530

    added changeing array element code

--- array.c
+++ array.c
@@ -1,9 +1,24 @@
-// array
+/*
+
+// array and accessing the array
 
 #include<stdio.h>
 int main ()
 {
     int a[] = {10,20,30,40,50};
-    printf("%d\n",a[4]*a[1]);
+    printf("%d\n",a[4]); // accessing the array
+    return 0;
+}
+
+*/
+
+// changing array element
+
+#include<stdio.h>
+int main ()
+{
+    int b[] = {10,20,30,40,50};
+    b[1]=22;
+    printf("To display the changed array element b[1] : %d\n",b[1]);
     return 0;
 }
