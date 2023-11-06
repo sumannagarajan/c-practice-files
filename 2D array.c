503f0fe7484eb190c4d255e7484117f8d2a1cca9
sumannagarajan <sumannagarajan@proton.me>
2023-11-06 22:02:06 +0530

    To display in matrix format

--- 2D array.c
+++ 2D array.c
@@ -1,16 +1,20 @@
+// 2D array 
+
+
 #include<stdio.h>
 int main ()
 {
     int a[3][3] = {{10,20,30},{40,50,60},{70,80,90}};
-    int i;
-    int j;
-    for ( i = 0; i < 3; i++)
+    int i,j;
+    printf("THE 3 X 3 MATRIX : \n");
+    for (i=0;i<3;i++)
     {
-    for (j = 0; j < 3; j++)
-    {
-    printf("VALUE a[%d][%d] = %d \n ",i,j,a[i][j]);
-    }
-    }
-    return 0;
+        for (j=0;j<3;j++)
+        {
+            printf("%d\t",a[i][j]);
+            
+        }
+        printf("\n");
+    }  
     
 }
