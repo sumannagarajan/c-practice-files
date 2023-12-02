c29cf1fb06d75493e4f820635c35ca59457cfa71
sumannagarajan <sumannagarajan@proton.me>
2023-12-02 19:25:43 +0530

    sting concatenate

---
+++ stringcon.c
@@ -0,0 +1,12 @@
+//sting concatenate
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str1[] = "suman";
+    char str2[] = "nagarajan";
+    strcat(str1,str2);
+    printf("%s\n",str1);
+    return 0;
+    
+}
