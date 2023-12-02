6bbb3b4d865919365b495f6089ae6fb7239ec617
sumannagarajan <sumannagarajan@proton.me>
2023-12-02 18:25:33 +0530

    string function

---
+++ stringfun.c
@@ -0,0 +1,11 @@
+//string function
+
+#include<stdio.h>
+#include<string.h> // must include in header for certain operation to perfrom in stings
+int main()
+{
+    char myname[] = "sumannagarajan";
+    printf("%ld\n", strlen(myname));
+    return 0;
+
+}
