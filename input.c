b3ce2653e366fdb388b139c25c71ff0cdc3ef839
sumannagarajan <sumannagarajan@proton.me>
2023-12-03 18:15:15 +0530

    input from user

---
+++ input.c
@@ -0,0 +1,11 @@
+//input from user
+
+#include<stdio.h>
+int main()
+{
+    int my_num;
+    printf("PLEASE ENTER THE NUMBER : \n");
+    scanf("%d",&my_num);
+    printf("your number is %d\n",my_num);
+    return 0;
+}
