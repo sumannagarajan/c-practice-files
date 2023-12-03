9912351a490e19c4623f02874a34982a16cad0fe
sumannagarajan <sumannagarajan@proton.me>
2023-12-03 20:58:30 +0530

    multiple inputs

---
+++ multipleinputs.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int my_num;
+    char my_char;
+    printf("PLEASE ENTER THE NUMBER AND CHARCTER AND PRESS ENTER : \n");
+    scanf("%d %c",&my_num,&my_char);
+    printf("your number and character is : %d %c\n",my_num,my_char);
+    return 0;
+
+}
