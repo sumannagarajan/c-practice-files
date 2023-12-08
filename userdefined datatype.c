87b07ade8e46092cb267dff381d88d15319b8a57
sumannagarajan <sumannagarajan@proton.me>
2023-12-08 18:50:48 +0530

    user defined data type is modifed by user satic and dynamic iputs

--- userdefined datatype.c
+++ userdefined datatype.c
@@ -1,3 +1,5 @@
+//user defined data type
+
 #include<stdio.h>
 #include<string.h>
 struct student
@@ -9,12 +11,30 @@ struct student
 int main()
 {
     struct student sd;
-    sd.roll_num=236;
-    sd.percentage=83.7;
-    strcpy(sd.name,"sumannagarjan");
+    struct student s;
+    
+    //static ini
+
+    sd.roll_num=236; 
+    sd.percentage=83.7; 
+    strcpy(sd.name,"sumannagarjan"); 
     printf("student rollno : %d\n",sd.roll_num);
     printf("student CGPA : %f\n",sd.percentage);
-    printf("student name : %s\n",sd.name);
+    printf("student name : %s\n\n",sd.name);
+
+    //dynamic ini (or) get user input
+
+    s.name;
+    s.roll_num;
+    s.percentage;
+    printf("please enter 2nd student details name,rollno,percentage :  \n");
+    fgets(s.name,sizeof(s.name),stdin);
+    scanf("%d",&s.roll_num);
+    scanf("%f",&s.percentage);
+    printf("student name : %s\n",s.name);
+    printf("student rollno : %d\n",s.roll_num);
+    printf("student CGPA : %0.1f\n",s.percentage);
+
     return 0;
     
 }
