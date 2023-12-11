0d1a3734584bc5e793a8a1374dca0a2f423a2f37
sumannagarajan <sumannagarajan@proton.me>
2023-12-11 21:30:16 +0530

    pointers

--- pointers.c
+++ pointers.c
@@ -6,8 +6,9 @@ int main()
     int age = 22;
     int *p = &age; //declation of pointer variable 
     printf("my age is : %d\n",age); // display the value store in age
-    printf("address of the age is %p\n",&age);
-    printf("To print the address of *p %p\n",p);
+    printf("address of the age is %p\n",&age); //display the address location variable age
+    printf("To print the address of age with pointer variable %p\n",p); //To display the address location with pointer variable (REFERENCE)
+    printf("To print the value of age with pointer %d\n",*p); //display the value of age with pointer (DEREFERENCE)
     return 0;
 
 }
