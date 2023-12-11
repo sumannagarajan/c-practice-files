93acba8ca630d528af626f08553f76ed0789281d
sumannagarajan <sumannagarajan@proton.me>
2023-12-11 22:49:13 +0530

    pointers accesing array element

--- pointerarray.c
+++ pointerarray.c
@@ -6,5 +6,8 @@ int main()
   
        printf("To print the address of 1st element in my_num variable %p\n",&my_num[0]);
        printf("To print the address of the variable my_num : %p\n",&my_num);
+       printf("%d\n",*my_num); // 1st element of an array
+       printf("%d\n",*(my_num +1)); // 2nd element of an array
+       printf("%d\n",*(my_num +2)); // 3rd element of an array
     return 0;
 }
