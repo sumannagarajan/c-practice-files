e8d9c31ea2a765f19b38ecf2aec3a697c1594099
sumannagarajan <sumannagarajan@proton.me>
2023-12-12 20:53:44 +0530

    pass array as function paramter

--- function_Arug_para.c
+++ function_Arug_para.c
@@ -1,3 +1,5 @@
+// paramaters & arguments in function
+/*
 #include<stdio.h>
 void myfun(char name[] , int age)
 {
@@ -9,6 +11,26 @@ int main()
     myfun("suman",22);
     myfun("vicky",22);
     myfun("krishna",23);
+    return 0;
+
+}
+
+*/
+
+#include<stdio.h>
+void myfun(int mynum[5])
+{
+    int i;
+    for ( i = 0; i < 5; i++)
+    {
+        printf("THE VALUE OF mynum[%d] = %d\n",i,mynum[i]);
+    }
+    
+}
+int main()
+{
+    int mynum[5] = {10,20,30,40,50};
+    myfun(mynum);
     return 0;
 
 }
