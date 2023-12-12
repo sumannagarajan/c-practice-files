aad744761652c029c15de2b1ebec928cd73b5ea6
sumannagarajan <sumannagarajan@proton.me>
2023-12-12 19:59:45 +0530

    function_arug_para

---
+++ function_Arug_para.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void myfun(char name[])
+{
+    printf("Hi my name is %s\n",name);
+
+}
+int main()
+{
+    myfun("suman");
+    myfun("vicky");
+    myfun("krishna");
+    return 0;
+    
+}
