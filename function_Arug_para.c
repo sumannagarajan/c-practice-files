86bc578b4fae2a8164b6cd6a33eca5291936a9f7
sumannagarajan <sumannagarajan@proton.me>
2023-12-12 20:06:30 +0530

    multiple aruguments

--- function_Arug_para.c
+++ function_Arug_para.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void myfun(char name[])
+void myfun(char name[] , int age)
 {
-    printf("Hi my name is %s\n",name);
+    printf("Hi my name is %s and iam %d year old \n",name,age);
 
 }
 int main()
 {
-    myfun("suman");
-    myfun("vicky");
-    myfun("krishna");
+    myfun("suman",22);
+    myfun("vicky",22);
+    myfun("krishna",23);
     return 0;
-    
+
 }
