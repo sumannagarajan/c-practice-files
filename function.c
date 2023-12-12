9df4486ec18f2d3d31c01d8c8eab7eaa899eb914
sumannagarajan <sumannagarajan@proton.me>
2023-12-12 13:07:05 +0530

    function decleartion

---
+++ function.c
@@ -0,0 +1,17 @@
+//function 
+
+#include<stdio.h>
+void myfun()           // function declearation     
+{
+    printf( "HI this is sumannagarajan this is my function & coding is fun \n");
+    int a=10;
+    int b=20;
+    printf("%d\n",a+b);
+}
+int main()
+{
+    myfun();     // we can also call the function multiple times when ever we want 
+    myfun();
+    myfun();
+    return 0;
+}
