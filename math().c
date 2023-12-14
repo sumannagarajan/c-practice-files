d9af871c243d691468721e78ae5a6db473b7567b
sumannagarajan <sumannagarajan@proton.me>
2023-12-14 22:27:52 +0530

    math function

---
+++ math().c
@@ -0,0 +1,13 @@
+// math function in c
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("%f\n",sqrt(10)); // square root function
+    printf("%f\n",ceil(10.9)); //upword roundoff
+    printf("%f\n",floor(10.9)); //downword roundoff
+    printf("%f\n",pow(5,5));
+    return 0;
+
+}
