b8c6348bd26ae379bb1c3748d8d0c5162cffd916
sumannagarajan <sumannagarajan@proton.me>
2023-12-17 18:56:44 +0530

    switch statement

--- switch.c
+++ switch.c
@@ -3,7 +3,9 @@
 
 // switch is like a menu based statement or system
 
+/*
 
+//static input 
 
 #include<stdio.h>
 int main ()
@@ -32,4 +34,44 @@ int main ()
         printf( "ERROR!!! YOU ENTER ABOVE THE VALUE OF 5 ......\n");
     }
     return 0;
+}
+
+*/
+
+
+// dynamic input from user
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("PLEASE ENTER THE NUMBER. THE ENTERED NUMBER SHOULD DISPLAY THE DAY OF THE WEEK : \n");
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+        printf("MONDAY \n");
+        break;
+    case 2:
+        printf("TUESDAY \n");
+        break;
+    case 3:
+        printf("WEDNESDAY \n");
+        break;
+    case 4:
+        printf("THURSDAY \n");
+        break;
+    case 5 :
+        printf("FRIDAY \n");
+        break;
+    case 6:
+        printf("SATURDAY \n");
+        break;
+    case 7:
+        printf("SUNDAY \n");
+        break;
+    default:
+        printf("SORRY YOU ENTER THE OUT OF RANGE NUMBER please enter 1 to 7 numbers \n");
+       break;
+    }
 }
